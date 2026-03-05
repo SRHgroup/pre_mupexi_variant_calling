2f7f899426340d6334a88abe10c61f144c0ad146
Grigorii Nos <nosgrigoriy@gmail.com>
2026-03-05 09:58:19 +0100

    Load Java 17 in default GATK module stacks

--- pipeline_defaults/toolchain.defaults.sh
+++ pipeline_defaults/toolchain.defaults.sh
@@ -10,9 +10,9 @@ module_anaconda="anaconda3/2025.06-1"
 module_java="java/17-openjdk"
 
 # Process-specific module sets
-modules_rna="ngs tools ${module_htslib} ${module_bcftools} ${module_gatk} ${module_anaconda}"
+modules_rna="ngs tools ${module_htslib} ${module_bcftools} ${module_java} ${module_gatk} ${module_anaconda}"
 modules_rna_bamfix="tools ${module_htslib} samtools/1.23"
 modules_gdna_hc="tools ngs ${module_java} ${module_gatk}"
 modules_gdna_post="tools ngs ${module_anaconda}"
 modules_dna_only_merge="ngs tools ${module_htslib} ${module_bcftools} ${module_anaconda}"
-modules_dna_only_phase="ngs tools ${module_htslib} ${module_bcftools} ${module_gatk} ${module_anaconda}"
+modules_dna_only_phase="ngs tools ${module_htslib} ${module_bcftools} ${module_java} ${module_gatk} ${module_anaconda}"
